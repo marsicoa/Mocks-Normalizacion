38b397407e07fbc2074edef47d80bb282b4f1dd6
marsicoa <50459011+marsicoa@users.noreply.github.com>
2022-08-24 21:00:28 -0300

    Add files via upload

---
+++ Mocks&Normalizacion/mocks/src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import productRouter from './routes/product.js';
+import cartRouter from './routes/cart.js';
+
+const PORT = 8080;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+//app.use('/api/productos', productRouter);
+app.use('/api/productos-test', productRouter);
+app.use('/api/carrito', cartRouter);
+
+const server = app.listen(PORT, () => {
+    console.log(` >>>>> Server corriendo en http://localhost:${PORT}`)
+    })
+    
+server.on('error', (err) => console.log(err));
